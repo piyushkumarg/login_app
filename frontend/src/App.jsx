9d3603235a1bf29efee7fba500c4fe501e2b8321
Piyush Kumar <83285872+piyushkumarg@users.noreply.github.com>
2023-06-07 12:20:11 +0530

    protected route added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,39 +10,49 @@ import Recovery from './components/Recovery'
 import Reset from './components/Reset'
 import PageNotFound from './components/PageNotFound'
 
+/** auth middleware */
+import { AuthorizeUser, ProtectRoute } from './middleware/auth'
 
 
 /**root routes */
 const router = createBrowserRouter([
-    {
-        path : '/',
-        element : <Username></Username>
-    },
-    {
-        path : '/register',
-        element : <Register></Register>
-    },
-    {
-        path : '/password',
-        element : <Password />
-    },
-    {
-        path : '/profile',
-        element : <Profile />
-    },
-    {
-        path : '/recovery',
-        element : <Recovery></Recovery>
-    },
-    {
-        path : '/reset',
-        element : <Reset></Reset>
-    },
-    {
-        path : '*',
-        element : <PageNotFound></PageNotFound>
-    },
-])
+  {
+    path: "/",
+    element: <Username></Username>,
+  },
+  {
+    path: "/register",
+    element: <Register></Register>,
+  },
+  {
+    path: "/password",
+    element: (
+      <ProtectRoute>
+        <Password />
+      </ProtectRoute>
+    ),
+  },
+  {
+    path: "/profile",
+    element: (
+      <AuthorizeUser>
+        <Profile />
+      </AuthorizeUser>
+    ),
+  },
+  {
+    path: "/recovery",
+    element: <Recovery></Recovery>,
+  },
+  {
+    path: "/reset",
+    element: <Reset></Reset>,
+  },
+  {
+    path: "*",
+    element: <PageNotFound></PageNotFound>,
+  },
+]);
 
 const App = () => {
   return (
